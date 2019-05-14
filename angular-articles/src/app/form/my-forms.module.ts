70183554356bc9a2b7f0f08aeae21a64e49246ef
Leandro Hereñú <leandrohere94@live.com.ar>
2019-05-13 22:05:56 -0300

    changes

--- angular-articles/src/app/form/my-forms.module.ts
+++ angular-articles/src/app/form/my-forms.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
 import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
 
 import { MenuBuilderFormComponent } from "./menu-builder-form/menu-builder-form.component";
+import { MenuItemListModule } from "../menu-item-list/menu-item-list.module";
 
 @NgModule({
   declarations: [MenuBuilderFormComponent],
@@ -17,8 +18,9 @@ import { MenuBuilderFormComponent } from "./menu-builder-form/menu-builder-form.
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatListModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule,
+    MenuItemListModule
   ],
   exports: [MenuBuilderFormComponent]
 })
